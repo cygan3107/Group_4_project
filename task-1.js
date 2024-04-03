dc378d4f5226d88ae7103ba1e4ce252655c8f865
cygan3107 <krzysztof.malon@onet.pl>
2024-04-03 17:16:01 +0200

    Summary

--- task-1.js
+++ task-1.js
@@ -1,1 +1,65 @@
 "use strict";
+/*let price = 0;
+const subscription = "pro";
+if (subscription === "pro") {
+    //true
+    price = 100;
+}
+console.log(price); // 100
+
+let price2 = 0;
+const subscription2 = "free";
+if (subscription2 === "pro") {
+    //false
+    price2 = 100;
+}
+console.log(price2); // 0
+
+const grade = 85;
+if (grade >= 70) {
+  console.log("Satisfactorily");
+} else {
+  console.log("Unsatisfactorily");
+}
+
+const grade = 40;
+if (grade >= 70) {
+  console.log("Satisfactorily");
+} else {
+  console.log("Unsatisfactorily");
+}
+
+if (condition_1) {
+  // kod, który jest wykonywany, jeśli warunek (condition_1) jest prawdziwy
+} else if (condition_2) {
+  // kod, który jest wykonywany, jeśli warunek (condition_2) jest prawdziwy
+} else if (condition_3) {
+  // kod, który jest wykonywany, jeśli warunek (condition_3) jest prawdziwy
+} else {
+	// kod, który jest wykonywany, jeśli wszystkie warunki są fałszywe
+}
+
+const grade = 85;
+if (grade >= 90) {
+  console.log("Perfectly");
+} else if (grade >= 80) {
+  console.log("Good");
+} else if (grade >= 70) {
+  console.log("Satisfactorily");
+} else {
+  console.log("Unsatisfactorily");
+}
+
+
+*/
+const grade = 85;
+
+if (grade >= 90) {
+    console.log("Perfectly");
+} else if (grade >= 86) {
+    console.log("Good");
+} else if (grade >= 70) {
+    console.log("Satisfactorily");
+} else {
+    console.log("Unsatisfactorily");
+}
