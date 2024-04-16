7e5bed9359a735e1dfe2b42d1aae77dd9d46caaa
cygan3107 <krzysztof.malon@onet.pl>
2024-04-16 18:44:14 +0200

    Update task-1.js

--- task-1.js
+++ task-1.js
@@ -1,4 +1,18 @@
 "use strict";
+function makeTransaction(quantity, pricePerDroid, customerCredits) {
+  const totalPrice = quantity * pricePerDroid;
+  if ( totalPrice <= customerCredits) {
+    console.log(`You ordered ${quantity} droids worth ${totalPrice} credits!`);
+  } else {
+    console.log(`Insufficient funds!`);
+  }
+}
+makeTransaction(5, 3000, 23000); // "You ordered 5 droids worth 15000 credits!"
+makeTransaction(3, 1000, 15000); // "You ordered 3 droids worth 3000 credits!"
+makeTransaction(10, 5000, 8000); // "Insufficient funds!"
+makeTransaction(8, 2000, 10000); // "Insufficient funds!"
+makeTransaction(10, 500, 5000); // "You ordered 10 droids worth 5000 credits!"
+
 /*let price = 0;
 const subscription = "pro";
 if (subscription === "pro") {
@@ -50,8 +64,6 @@ if (grade >= 90) {
   console.log("Unsatisfactorily");
 }
 
-
-*/
 const grade = 85;
 
 if (grade >= 90) {
@@ -62,4 +74,4 @@ if (grade >= 90) {
     console.log("Satisfactorily");
 } else {
     console.log("Unsatisfactorily");
-}
+}*/
