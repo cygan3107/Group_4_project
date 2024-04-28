ec73a9294ce1f5e29a46e6c07863467a7c18d52b
cygan3107 <krzysztof.malon@onet.pl>
2024-04-28 09:52:45 +0200

    Summary

--- task-2.js
+++ task-2.js
@@ -1,9 +1,15 @@
 "use strict";
 function formatMessage(message, maxLength) {
-    const totalPrice = quantity * pricePerDroid;
-    if ( totalPrice <= customerCredits) {
-      console.log(`You ordered ${quantity} droids worth ${totalPrice} credits!`);
-    } else {
-      console.log(`Insufficient funds!`);
-    }
+  if (message.length <= maxLength) {
+    return message;
   }
+  else {
+    return message.slice(0, maxLength).padEnd(maxLength + 3, ".");
+  }
+}
+console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
+console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
+console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
+console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
